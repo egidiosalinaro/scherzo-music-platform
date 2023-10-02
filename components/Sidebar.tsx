819a2e605a1d14c881b255e71583f1817192d6b7
egy351 <129901135+egy351@users.noreply.github.com>
2023-10-02 12:00:29 +0200

    initial commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: 'Home',
+        active: pathname !== '/search',
+      },
+    ],
+    []
+  );
+
+  return <div>{children}</div>;
+};
+
+export default Sidebar;
