185333b767e9200da51f3e019e9fd51e9f5a5096
egy351 <129901135+egy351@users.noreply.github.com>
2023-10-06 20:50:27 +0200

    songitem component and get requestes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['ajfrttjnrbjclwqrqxpz.supabase.co'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
